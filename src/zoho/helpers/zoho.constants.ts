2c9350589dd945b565ed37eaaddaef874808f134
Prabhas Miraki <prabhasr@mirakitech.com>
2026-07-20 12:09:26 +0530

    issue

--- src/zoho/helpers/zoho.constants.ts
+++ src/zoho/helpers/zoho.constants.ts
@@ -4,3 +4,5 @@ export const ZOHO_DEFAULT_ACCOUNTS_URL = 'https://accounts.zoho.in';
 export const ZOHO_TOKEN_REFRESH_SKEW_MS = 2 * 60 * 1000;
 export const ZOHO_DEFAULT_RETRY_ATTEMPTS = 3;
 export const ZOHO_DEFAULT_RETRY_DELAY_MS = 750;
+/** Express lowercases incoming headers; Zoho HMAC signature header name. */
+export const ZOHO_WEBHOOK_SIGNATURE_HEADER = 'x-zoho-webhook-signature';
